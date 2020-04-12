1eb4a2726fc120b5d5d00109bdf3947a5e2991fc
Paulo Henrique <paulo.salkys@bravi.com.br>
2020-04-12 17:07:34 -0300

    Commit inicial da versao 1.0 do blog do Haio

---
+++ blog/configuracao.php
@@ -0,0 +1,7 @@
+<?php
+# URL DO SISTEMA DE NOTICIAS
+#const urlnoticias = 'http://192.168.15.19:31822';
+#const urlnoticias = 'http://192.168.15.19:8587';
+const urlnoticias = 'http://192.168.99.100:31822';
+
+?>
---
+++ crud/enviarMensagens.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#curl -X POST -H 'Content-type: alication/json' --data '{"text":"Olá, um novo pod acabou de ser criado!"}' https://hooks.slack.com/services/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
+
+curl -X POST -H 'Content-type: application/json' --data '{"text":"ALERT!!! BLOG do H A I O BOMBANDO! Um novo pod acabou de ser criado!!"}' https://hooks.slack.com/services/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
